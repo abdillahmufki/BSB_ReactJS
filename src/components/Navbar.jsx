2c8cad37a561f36d3afccf268acd3a7262cd3005
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-06-05 16:56:57 +0700

    update logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,98 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo/bsbBlue.png";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const visible = prevScrollPos > currentScrollPos;
+
+      setShowNavbar(visible);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <>
-      <div className="navbar bg-[#fff] text-black px-5 py-4 z-50">
-        <div className="navbar-start">
-          <div className="dropdown text-lg">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-[#000] text-white z-50 rounded-box w-52"
+    <div
+      className={`navbar bg-white text-black px-5 py-4 z-50 text-lg fixed top-0 w-full transition-all duration-300 ${
+        showNavbar ? "" : "-translate-y-full"
+      } ${showNavbar ? "shadow-md" : ""}`}
+    >
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm text-base dropdown-content mt-3 p-2 shadow bg-[#000] text-white z-50 rounded-box w-52"
+          >
+            <li>
+              <Link to="/">
                 <a>Home</a>
-              </li>
-              <li>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
                 <a>About</a>
-              </li>
-              <li>
+              </Link>
+            </li>
+            <li>
+              <Link to="/products">
                 <a>Products</a>
-              </li>
-              <li tabIndex={0}>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2 bg-black">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost normal-case text-xl">
-            <img src={logo} alt="logo" width={100} />
-          </a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Products</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-black text-black shadow-lg">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              </Link>
             </li>
             <li>
               <a>Item 3</a>
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <Button text="Contact Us" />
-        </div>
+        <a className="btn btn-ghost normal-case text-xl">
+          <img src={logo} alt="logo" width={100} />
+        </a>
       </div>
-    </>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal text-base px-1">
+          <li>
+            <Link to="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link to="/products">
+              <a>Products</a>
+            </Link>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Button text="Contact Us" />
+      </div>
+    </div>
   );
 };
 
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="hero min-h-screen bg-[#fff]">
+    <div className="hero min-h-screen bg-[#fff] mt-10">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <img src={image} className="mb-10" alt="error image" width={500} />
