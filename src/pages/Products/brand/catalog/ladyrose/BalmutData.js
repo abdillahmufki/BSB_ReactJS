1bb36c26ad45b03ac189f250b9c84cc1d946e592
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-06-22 15:56:45 +0700

    develop product

--- src/pages/Products/brand/catalog/ladyrose/BalmutData.js
+++ src/pages/Products/brand/catalog/ladyrose/BalmutData.js
@@ -3,7 +3,7 @@ import Alex from "../../../../../assets/catalogue/itg_lady_rose/balmut/Alex.jpg"
 import Alodia from "../../../../../assets/catalogue/itg_lady_rose/balmut/Alodia.jpg";
 import Anela from "../../../../../assets/catalogue/itg_lady_rose/balmut/Anela.jpg";
 import AstroBear from "../../../../../assets/catalogue/itg_lady_rose/balmut/Astrobear.jpg";
-import banda from "../../../../../assets/catalogue/itg_lady_rose/balmut/banda.jpg";
+import banda from "../../../../../assets/catalogue/itg_lady_rose/balmut/Banda.jpg";
 import batman from "../../../../../assets/catalogue/itg_lady_rose/balmut/batman.jpg";
 import Celine from "../../../../../assets/catalogue/itg_lady_rose/balmut/Celine.jpg";
 import Chelsea_Blue from "../../../../../assets/catalogue/itg_lady_rose/balmut/Chelsea_Blue.jpg";
