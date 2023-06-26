1ea2df325be636b8184518e2d19635fa832c6c4e
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-06-26 09:39:43 +0700

    develop products

--- src/pages/Products/brand/catalog/ladyrose/BedCoverSingleData.js
+++ src/pages/Products/brand/catalog/ladyrose/BedCoverSingleData.js
@@ -2,35 +2,51 @@ import Aldo from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Aldo.jp
 import Alex from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Alex.jpg";
 import astrobear from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/astrobear.jpg";
 import batman from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/batman.jpg";
-import chealseblue from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/chelsea_blue.jpg";
+import chelsea_blue from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/chelsea_blue.jpg";
 import dawson from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/dawson.jpg";
 import dc_girls from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/dc_girls.jpg";
 import dc_super_friends from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/dc_super_friends.jpg";
 import delia from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/delia.jpg";
-import Doraemon_Comic from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/doraemon_comic.jpg";
-import Doraemon_hopter from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/doraemon_hopter.jpg";
+import doraemon_ from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/doraemon_.jpg";
+import doraemon_comic from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/doraemon_comic.jpg";
+import doraemon_hopter from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/doraemon_hopter.jpg";
+import doraemon_sea from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/doraemon_sea.jpg";
+import FC_Barcelona from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/FC_Barcelona.jpg";
+import FC_Juventus from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/FC_Juventus.jpg";
 import Felix from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Felix.jpg";
+import Ferren from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Ferren.jpg";
+import Flamingo from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Flamingo.jpg";
 import furr from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/furr.jpg";
 import GC from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/GC.jpg";
+import Kerokeroppi from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Kerokeroppi.jpg";
+import Keroppi_chef from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Keroppi_chef.jpg";
 import Kitty_Cake from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Kitty_Cake.jpg";
+import Kitty_Cappucino from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Kitty_Cappucino.jpg";
 import Leon from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Leon.jpg";
-import Lolita from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/lolita.jpg";
+import little_pony from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/little_pony.jpg";
+import little_twin_star from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/little_twin_star.jpg";
+import LOL from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/LOL.jpg";
+import lolita from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/lolita.jpg";
+import london from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/london.jpg";
+import Louis from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Louis.jpg";
+import Maurin from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Maurin.jpg";
+import Monstera from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Monstera.jpg";
 import MU_Red from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/MU_Red.jpg";
-import Parisian from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Parisian.jpg";
+import parisian from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/parisian.jpg";
 import Peter from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Peter.jpg";
 import Pikka from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Pikka.jpg";
-import Polka_Grey from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Polka_Grey.jpg";
-import Regan from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Regan.jpg";
-import Scarlet from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Scarlet.jpg";
-import runabouts from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/runabouts.jpg";
-import royal_princess from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/royal_princess.jpg";
+import polka_grey from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/polka_grey.jpg";
 import Rabbit_Star from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Rabbit_Star.jpg";
-import Shinta from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Shinta.jpg";
-import Star from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Star.jpg";
+import Regan from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Regan.jpg";
+import royal_princess from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/royal_princess.jpg";
+import runabouts from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/runabouts.jpg";
+import scarlet from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/scarlet.jpg";
+import shinta from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/shinta.jpg";
+import star from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/star.jpg";
 import superman from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/superman.jpg";
 import sweety_pony from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/sweety_pony.jpg";
 import Vania from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/Vania.jpg";
-import william from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/William.jpg";
+import William from "../../../../../assets/catalogue/itg_lady_rose/bcsingle/William.jpg";
 
 export const BedCoverSingleData = [
   {
@@ -58,9 +74,9 @@ export const BedCoverSingleData = [
     type: "Bed Cover Single",
   },
   {
-    name: "Chelsea Blue",
-    alt: "Chelsea Blue",
-    src: chealseblue,
+    name: "Chelsea",
+    alt: "Chelsea",
+    src: chelsea_blue,
     type: "Bed Cover Single",
   },
   {
@@ -88,21 +104,57 @@ export const BedCoverSingleData = [
     type: "Bed Cover Single",
   },
   {
+    name: "Doraemon",
+    alt: "Doraemon",
+    src: doraemon_,
+    type: "Bed Cover Single",
+  },
+  {
     name: "Doraemon Comic",
     alt: "Doraemon Comic",
-    src: Doraemon_Comic,
+    src: doraemon_comic,
     type: "Bed Cover Single",
   },
   {
     name: "Doraemon Hopter",
     alt: "Doraemon Hopter",
-    src: Doraemon_hopter,
+    src: doraemon_hopter,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Doraemon Sea",
+    alt: "Doraemon Sea",
+    src: doraemon_sea,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Barcelona",
+    alt: "Barcelona",
+    src: FC_Barcelona,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Juventus",
+    alt: "Juventus",
+    src: FC_Juventus,
     type: "Bed Cover Single",
   },
   {
     name: "Felix",
     alt: "Felix",
     src: Felix,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Ferren",
+    alt: "Ferren",
+    src: Ferren,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Flamingo",
+    alt: "Flamingo",
+    src: Flamingo,
     type: "Bed Cover Single",
   },
   {
@@ -118,9 +170,27 @@ export const BedCoverSingleData = [
     type: "Bed Cover Single",
   },
   {
-    name: " Kitty Cake",
-    alt: " Kitty Cake",
+    name: "Kerokeroppi",
+    alt: "Kerokeroppi",
+    src: Kerokeroppi,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Keroppi Chef",
+    alt: "Keroppi Chef",
+    src: Keroppi_chef,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Kitty Cake",
+    alt: "Kitty Cake",
     src: Kitty_Cake,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Kitty Cappucino",
+    alt: "Kitty Cappucino",
+    src: Kitty_Cappucino,
     type: "Bed Cover Single",
   },
   {
@@ -130,21 +200,63 @@ export const BedCoverSingleData = [
     type: "Bed Cover Single",
   },
   {
-    name: "Lolita",
-    alt: "Lolita",
-    src: Lolita,
+    name: "Little Pony",
+    alt: "Little Pony",
+    src: little_pony,
     type: "Bed Cover Single",
   },
   {
-    name: "Manchester United",
-    alt: "Manchester United",
+    name: "Little Twin Star",
+    alt: "Little Twin Star",
+    src: little_twin_star,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "LOL",
+    alt: "LOL",
+    src: LOL,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "lolita",
+    alt: "lolita",
+    src: lolita,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "london",
+    alt: "london",
+    src: london,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Louis",
+    alt: "Louis",
+    src: Louis,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Maurin",
+    alt: "Maurin",
+    src: Maurin,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Monstera",
+    alt: "Monstera",
+    src: Monstera,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Menchester United",
+    alt: "Menchester United",
     src: MU_Red,
     type: "Bed Cover Single",
   },
   {
     name: "Parisian",
     alt: "Parisian",
-    src: Parisian,
+    src: parisian,
     type: "Bed Cover Single",
   },
   {
@@ -162,31 +274,7 @@ export const BedCoverSingleData = [
   {
     name: "Polka Grey",
     alt: "Polka Grey",
-    src: Polka_Grey,
-    type: "Bed Cover Single",
-  },
-  {
-    name: "Regan",
-    alt: "Regan",
-    src: Regan,
-    type: "Bed Cover Single",
-  },
-  {
-    name: "Scarlet",
-    alt: "Scarlet",
-    src: Scarlet,
-    type: "Bed Cover Single",
-  },
-  {
-    name: "Runabouts",
-    alt: "Runabouts",
-    src: runabouts,
-    type: "Bed Cover Single",
-  },
-  {
-    name: "Royal Princess",
-    alt: "Royal Princess",
-    src: royal_princess,
+    src: polka_grey,
     type: "Bed Cover Single",
   },
   {
@@ -196,15 +284,39 @@ export const BedCoverSingleData = [
     type: "Bed Cover Single",
   },
   {
+    name: "Regan",
+    alt: "Regan",
+    src: Regan,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Royal Princess",
+    alt: "Royal Princess",
+    src: royal_princess,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Runabouts",
+    alt: "Runabouts",
+    src: runabouts,
+    type: "Bed Cover Single",
+  },
+  {
+    name: "Scarlet",
+    alt: "Scarlet",
+    src: scarlet,
+    type: "Bed Cover Single",
+  },
+  {
     name: "Shinta",
     alt: "Shinta",
-    src: Shinta,
+    src: shinta,
     type: "Bed Cover Single",
   },
   {
     name: "Star",
     alt: "Star",
-    src: Star,
+    src: star,
     type: "Bed Cover Single",
   },
   {
@@ -228,7 +340,7 @@ export const BedCoverSingleData = [
   {
     name: "William",
     alt: "William",
-    src: william,
+    src: William,
     type: "Bed Cover Single",
   },
 ];
