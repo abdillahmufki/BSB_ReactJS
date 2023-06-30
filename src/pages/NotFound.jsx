456f32ba7e4330f519531782a986be0257994cb0
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-06-30 13:14:00 +0700

    develop products rainbow

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,29 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import image from "../assets/content/404.gif";
-import { Button } from "../components";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       navigate("/");
-    }, 5000);
+    }, countdown * 1000);
 
     return () => {
       clearTimeout(timeout);
     };
-  }, [navigate]);
+  }, [countdown, navigate]);
+
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [countdown]);
 
   return (
     <div className="hero mt-14 min-h-screen bg-[#fff]">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <img src={image} className="mb-10" alt="error image" width={500} />
-          <Link to="/">
-            <Button>Back to Home</Button>
-          </Link>
+          <span className="countdown font-mono text-4xl">
+            <span style={{ "--value": countdown }}></span> Detik
+          </span>
+          <div className="my-3">
+            <h1 className="mb-5 text-3xl font-bold">
+              Untuk Kembali Ke Beranda
+            </h1>
+          </div>
         </div>
       </div>
     </div>
